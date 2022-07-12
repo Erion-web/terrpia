6054756d22b9641140b107fc7f212ec3e05a2c36
Erion Vrella <lonilisi333@gmail.com>
2022-07-12 22:12:52 +0200

    added features on Veggie and Popular

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Category />
+        <Pages />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./Home";
+import Cuisine from "./Cuisine";
+import { Route, Routes } from "react-router-dom";
+
+function Pages() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine" element={<Cuisine />} />
+    </Routes>
+  );
+}
+
+export default Pages;
